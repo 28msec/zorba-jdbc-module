4e2b9b6aafd5443a065ef8702e4e84705c1b9110
chillery+launchpad@lambda.nu <>
2013-01-03 02:58:09 -0800

    Revamp setup script to not require root, and install/run entirely from user-specific directory.

--- setup/mysql-setup.sh
+++ setup/mysql-setup.sh
@@ -1,54 +1,66 @@
 #!/bin/bash
 
-while getopts distch: opt
+# Default location to dump stuff and create databases
+WORKDIR=~/jdbc-module-mysql
+
+# MySQL'd "datadir"
+DATADIR=${WORKDIR}/datadir
+
+# Location for the logs
+LOGDIR=${WORKDIR}/log
+
+while getopts distchw: opt
 do
   case "$opt" in
+        w)
+            WORKDIR="$OPTARG"
+            echo "Data directory: $WORKDIR"
+            ;;
         d)
-            echo "Download" 
+            echo "Download"
+            mkdir -p $WORKDIR
+            cd $WORKDIR
             wget http://cdn.mysql.com/Downloads/MySQL-5.5/mysql-5.5.28-linux2.6-x86_64.tar.gz
             wget http://cdn.mysql.com/Downloads/Connector-J/mysql-connector-java-5.1.22.tar.gz
             ;;
         i)
             echo "Install"
+            (
+            cd $WORKDIR
             tar -xvzf mysql-5.5.28-linux2.6-x86_64.tar.gz
             tar -xvzf mysql-connector-java-5.1.22.tar.gz
             mv mysql-5.5.28-linux2.6-x86_64 mysql
             sudo apt-get install libaio1
-            sudo mkdir -p /var/lib/mysql_rq
-            sudo chown -R mysql.mysql /var/lib/mysql_rq
-            sudo mkdir -p /var/log/mysql_rq
-            sudo chown -R mysql.mysql /var/log/mysql_rq
-            
-            sudo ./mysql/scripts/mysql_install_db --user=mysql --datadir=/var/lib/mysql_rq --basedir=./mysql
-            sudo cp ./mysql/bin/mysqld ./mysql/bin/mysqld_rq
-            sudo cp my.cnf /var/log/mysql_rq/my.cnf
+            mkdir -p $DATADIR $LOGDIR $DATADIR/innodb_log
+            ./mysql/scripts/mysql_install_db --user=$USER --datadir=$DATADIR --basedir=./mysql --no-defaults
+            )
+            MODDIR=`dirname $0`
+            cp $MODDIR/my.cnf $DATADIR/my.cnf
             ;;
         s)
             echo "Start"
-            cd mysql
-            sudo ./bin/mysqld_safe --defaults-file=/var/log/mysql_rq/my.cnf --user=mysql --datadir=/var/lib/mysql_rq/ --port=3377 --mysqld=mysqld_rq  --log-error=/var/log/mysql_rq/log.err &
+            cd $WORKDIR/mysql
+            ./bin/mysqld_safe --defaults-file=$DATADIR/my.cnf --user=$USER --pid-file=$DATADIR/mysql_rq.pid --datadir=$DATADIR --log-error=$LOGDIR/log.err &
             cd ..
             ;;
         t)
             echo "Stop"
-            cd mysql
-            sudo ./bin/mysqladmin -S /var/lib/mysql_rq/mysql.sock shutdown &
-            cd ..
+            kill `cat $DATADIR/mysql_rq.pid`   
             ;;
         c)
             echo "Clean everything"
+            cd $WORKDIR
             rm -fR ./mysql
             rm -fR ./mysql-connector-java-5.1.22
-            sudo -fR /var/lib/mysql_rq
-            sudo -fR /var/log/mysql_rq
             ;;            
         h)
-            echo "usage: sudo $0 [-distcvh] "
+            echo "usage: $0 [-distcvh] [-w path]"
             echo "       -d  Download MySQL DB"
             echo "       -i  Install"
             echo "       -s  Start service"
             echo "       -t  Stop service"
             echo "       -c  Clean everything"
+            echo "       -w path  Path to store everything (default ~/jdbc-module-mysql)"
             echo "       -h  Help"
             exit 2
             ;;
